8b4bdcb4c51a7ac4eea9caab8161a2c1b8553f3c
Adam DePaolo <amdepaolo@gmail.com>
2022-03-19 22:00:49 -0400

    commit tree f416abe1a453b4fcefcad3be77a3b4a95e13863b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    let firstTwo = [drivers[0],drivers[1]];
+    return firstTwo;
+};
+
+const returnLastTwoDrivers = function(drivers){
+    let lastTwo = [drivers[drivers.length-2],drivers[drivers.length-1]];
+    return lastTwo;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        let newfare = fare*multiplier;
+        return newfare;
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, choice){
+    return choice(arrayOfDrivers);
+}
